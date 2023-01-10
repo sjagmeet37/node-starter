6c131a7832443efc95f751d42009a69e52c840a1
Jagmeet Sandhu <sjagmeet37@gmail.com>
2023-01-10 19:20:58 +0530

    adding server.js and app.js

---
+++ server.js
@@ -0,0 +1,10 @@
+const http = require("http");
+const app = require("./app");
+const server = http.createServer(app);
+
+
+const port =  3000;
+
+server.listen(port, () => {
+  console.log(`Server is running on http://localhost:${port}`);
+});
